5bdf7a134914cb292f5df964ee1afd17cedafc00
Kevin Carrogan <kevin@carrogan.com>
2023-06-16 16:37:38 +0100

    Use content negotiation to respond to cats endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,20 +8,24 @@ const app = express();
 app.use(cors());
 
 app.get("/cats/", async (req, res) => {
-  const cats = await getCats();
-  res.json(cats);
-});
+  res.format({
+    "application/json": async () => {
+      const cats = await getCats();
+      res.json(cats);
+    },
+    "text/event-stream": async () => {
+      res.setHeader("Content-Type", "text/event-stream");
+      res.setHeader("Cache-Control", "no-cache");
+      res.setHeader("Connection", "keep-alive");
+      res.setHeader("Transfer-Encoding", "chunked");
 
-app.get("/cats/locations/stream/", (req, res) => {
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
-  res.setHeader("Transfer-Encoding", "chunked");
-
-  setInterval(async () => {
-    const locations = await getLocations();
-    res.write(`data: ${JSON.stringify(locations)}\n\n`);
-  }, 5000);
+      setInterval(async () => {
+        const locations = await getLocations();
+        res.write(`data: ${JSON.stringify(locations)}\n\n`);
+      }, 5000);
+    },
+    default: () => res.status(406).send("Not acceptable"),
+  });
 });
 
 await connectToTracker();
